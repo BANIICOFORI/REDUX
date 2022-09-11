5b5ab676246d7f224785efd26eab4fd02d43e6a1
BANIICOFORI <baniicofori@gmail.com>
2022-09-11 23:59:10 +0000

    First Commit

---
+++ src/Components/Home.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Button from 'react-bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+function Home() {
+  return (
+    <div>
+       
+    </div>
+  )
+}
+
+export default Home;
